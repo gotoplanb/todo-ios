e44250aeeb6e9dd4de96bd41aa972c5d3de8d1f5
Dave Stanton <davestanton.us@gmail.com>
2025-04-19 11:37:45 -0400

    Initial Commit

---
+++ todoTests/todoTests.swift
@@ -0,0 +1,17 @@
+//
+//  todoTests.swift
+//  todoTests
+//
+//  Created by Dave Stanton on 4/19/25.
+//
+
+import Testing
+@testable import todo
+
+struct todoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
